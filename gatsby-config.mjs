aaf3e287e0452fca3095f731077ce95062f0e0bb
Jeremy Fleming <jeremyfleming281@gmail.com>
2026-06-03 00:23:41 -0400

    remove icon

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -12,12 +12,6 @@ const config = {
     "gatsby-plugin-postcss",
     "gatsby-plugin-sitemap",
     {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    {
       resolve: "gatsby-plugin-mdx",
       options: {
         extensions: [".mdx", ".md"],
