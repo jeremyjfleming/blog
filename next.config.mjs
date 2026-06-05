3427e61346cab34b6f744084c12291b465ddfb0e
Jeremy Fleming <jeremyfleming281@gmail.com>
2026-06-05 11:31:40 -0400

    change rootpath

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/p',
+  basePath: '/blog',
   outputFileTracingRoot: __dirname,
 }
 export default nextConfig
